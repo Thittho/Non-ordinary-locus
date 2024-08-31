ad456d770e79d65d5797e4b1cb04c10acdff272f
Thittho <bouchetthomas@orange.fr>
2024-08-31 15:49:09 +0200

    added some data

--- magma/example_complete.m
+++ magma/example_complete.m
@@ -1,14 +1,14 @@
 Attach("~/github/Non-ordinary-locus/magma/data_code.m");
 
-for p in PrimesInInterval(17,17) do
+for p in PrimesInInterval(29,29) do
     res := [0,0,0,0,0];
 
-    for i0 in [0..p-1] do
+    for i0 in [25..28] do
         "i0 := ", i0;
         p_r := CurveDataForPComplete(p, 1 : i0 := i0);
         "p-rank data:", p_r;
         res := [res[i]+p_r[i] : i in [1..5]];
     end for;
     res;
-    //Write("~/p_rank_fp_complete_new.m", "[*" cat Sprint(p) cat ", 1, " cat Sprint(res) cat "*],");
+    Write("~/p_rank_fp_complete_new.m", "[*" cat Sprint(p) cat ", 1, " cat Sprint(res) cat "*],");
 end for;
