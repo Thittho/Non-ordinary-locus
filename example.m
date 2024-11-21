2189ee37e2ca565c6d4671c5d1fa710530305b62
Thittho <bouchetthomas@orange.fr>
2024-11-21 11:07:48 +0100

    new data

--- example.m
+++ example.m
@@ -1,11 +1,11 @@
 Attach("~/github/Non-ordinary-locus/tools.m");
 
 r := 1;
-g := 5;
+g := 3;
 s := 10000000; // put s to 0 if you want the whole family
 doc := "~/github/Non-ordinary-locus/data/genus-" cat Sprint(g) cat "/p_rank_fp" cat Sprint(r) cat ".m";
 
-for p in PrimesInInterval(240,500) do
+for p in PrimesInInterval(900,1000) do
   if (2*g+1) mod p ne 0 then
     p;
     res := CurveDataForP(g, p, r : s := s);
