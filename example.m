ba175a4a45eab9b97ce8cd2c3d344fc9af3f1a27
Thittho <bouchetthomas@orange.fr>
2025-03-16 13:13:50 +0100

    final data + higher genera

--- example.m
+++ example.m
@@ -1,11 +1,11 @@
 Attach("~/github/Non-ordinary-locus/tools.m");
 
-r := 3;
-g := 5;
-s := 100000000; // put s to 0 if you want the whole family
-doc := "~/github/Non-ordinary-locus/data/7_dim_family/genus-" cat Sprint(g) cat "/p_rank_fp" cat Sprint(r) cat ".m";
+r := 2;
+g := 20;
+s := 1000000; // put s to 0 if you want the whole family
+doc := "~/p_rank_fp" cat Sprint(r) cat "g" cat Sprint(g) cat ".m";
 
-for p in PrimesInInterval(67,67) do
+for p in PrimesInInterval(3,100) do
   if (2*g+1) mod p ne 0 then
     p;
     res := CurveDataForP(g, p, r : s := s);
--- example_galois.m
+++ example_galois.m
@@ -1,19 +1,20 @@
 load "galois.m";
 
-g := 4; // g is the genus
+g := 5; // g is the genus
 r := 3; // r is the power of p
 s := 100000000; // s is the number of samples
 
 doc := "~/github/Non-ordinary-locus/data/galois_family/genus-" cat Sprint(g) cat "/p_rank_fp" cat Sprint(r) cat ".m";
 
-for p in PrimesInInterval(5,5) do
+for p in PrimesInInterval(97,97) do
     printf "p = %o\n", p;
     time p_ranks := CurveDataForPGalois(g, p, r, s);
     printf "p-rank data = %o\n", p_ranks;
     non_ord := &+p_ranks[1..g];
     tot := &+p_ranks;
     if tot ne 0 then
+        "p = ", p;
         RealField(20)!(p^r * non_ord/tot);
-        //Write(doc, [p, r, tot] cat p_ranks);
+        Write(doc, [p, r, tot] cat p_ranks);
     end if;
 end for;
